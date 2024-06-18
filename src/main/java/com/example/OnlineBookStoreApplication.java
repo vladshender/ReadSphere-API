42bab353127367184261ccfae3cbc36ce30bef6c
Vlad <mazafackery@gmail.com>
2024-06-18 09:13:44 +0300

    implemented book revert

--- src/main/java/com/example/OnlineBookStoreApplication.java
+++ src/main/java/com/example/OnlineBookStoreApplication.java
@@ -1,35 +1,13 @@
 package com.example;
 
-import com.example.model.Book;
-import com.example.service.BookService;
-import java.math.BigDecimal;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
 
-    @Autowired
-    private BookService bookService;
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookStoreApplication.class, args);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(OnlineBookStoreApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            Book book1 = new Book();
-            book1.setPrice(BigDecimal.valueOf(123));
-            book1.setTitle("1984");
-            book1.setAuthor("Oruel");
-            book1.setDescription("Future world in book");
-
-            bookService.save(book1);
-            System.out.println(bookService.findAll());
-        };
-    }
 }
