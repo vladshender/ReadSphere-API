de2e7cdea41a784de74a9dca122b3ba4b6513d59
Vlad <mazafackery@gmail.com>
2024-06-24 10:43:33 +0300

    added method toDtoList

--- src/main/java/com/example/mapper/BookMapper.java
+++ src/main/java/com/example/mapper/BookMapper.java
@@ -4,6 +4,7 @@ import com.example.config.MapperConfig;
 import com.example.dto.BookDto;
 import com.example.dto.CreateBookRequestDto;
 import com.example.model.Book;
+import java.util.List;
 import org.mapstruct.Mapper;
 
 @Mapper(config = MapperConfig.class)
@@ -11,4 +12,6 @@ public interface BookMapper {
     BookDto toDto(Book book);
 
     Book toModel(CreateBookRequestDto requestDto);
+
+    List<BookDto> toDtoList(List<Book> books);
 }
