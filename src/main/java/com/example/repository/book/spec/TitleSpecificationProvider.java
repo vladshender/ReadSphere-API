4efceb40ea404e3d66663862a6d585ea7afab9b9
Vlad <mazafackery@gmail.com>
2024-07-26 15:08:54 +0300

    added hw book specification

---
+++ src/main/java/com/example/repository/book/spec/TitleSpecificationProvider.java
@@ -0,0 +1,20 @@
+package com.example.repository.book.spec;
+
+import com.example.model.Book;
+import com.example.repository.SpecificationProvider;
+import java.util.Arrays;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TitleSpecificationProvider implements SpecificationProvider<Book> {
+    @Override
+    public String getKey() {
+        return "title";
+    }
+
+    public Specification<Book> getSpecification(String[] params) {
+        return (root, query, criteriaBuilder) -> root.get("title")
+                .in(Arrays.stream(params).toArray());
+    }
+}
